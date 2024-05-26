dd93138d406675de8d67ed31956b1870a0c59ef6
Omar-Alme <omar.almehdar17@gmail.com>
2024-05-26 23:05:50 +0200

    add theme functionality, light/dark mode

--- rento/src/components/NavLinks.tsx
+++ rento/src/components/NavLinks.tsx
@@ -1,10 +1,25 @@
+import { links } from '@/utils';
+import { NavLink } from 'react-router-dom';
 
 function NavLinks() {
   return (
-    <div>
-      Navlinks
+    <div className='hidden lg:flex justify-center items-center gap-x-4'>
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            className={({ isActive }) => {
+              return `capitalize font-light tracking-wide ${
+                isActive ? 'text-primary' : ''
+              }`;
+            }}
+            key={link.label}
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
     </div>
-  )
+  );
 }
-
-export default NavLinks
+export default NavLinks;
--- rento/src/features/theme/themeSlice.ts
+++ rento/src/features/theme/themeSlice.ts
@@ -1,12 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { applyTheme } from '@/utils';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  name: 'theme slice',
+export type Theme = 'dark' | 'light' | 'system'
+
+
+type ThemState = {
+  theme: Theme;
+};
+
+const initializeTheme = ():Theme =>{
+  const theme = localStorage.getItem('theme') as Theme || 'system'
+  applyTheme(theme)
+  return theme;
+}
+
+const initialState:ThemState = {
+  theme: initializeTheme(),
 };
 const themeSlice = createSlice({
   name: 'theme',
   initialState,
-  reducers: {},
+  reducers: {
+    setTheme:(state,action: PayloadAction<Theme>)=>{
+      state.theme = action.payload
+      applyTheme(action.payload)
+      localStorage.setItem('theme', action.payload);
+    }
+  },
 });
+
+export const { setTheme } = themeSlice.actions;
 
 export default themeSlice.reducer;
