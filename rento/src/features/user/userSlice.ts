831ee50523dcff5728d1f3c3aa53c07b55c3fac4
Omar-Alme <omar.almehdar17@gmail.com>
2024-06-13 20:36:05 +0200

    add userslice

--- rento/src/features/user/userSlice.ts
+++ rento/src/features/user/userSlice.ts
@@ -1,12 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { toast } from '@/components/ui/use-toast'
 
-const initialState = {
-  name: 'user slice',
+
+export type User = {
+  username: string;
+  jwt: string;
+}
+
+type UserState = {
+  user: User | null
+}
+
+const getUserFromLocalStorage = (): User | null => {
+  const user = localStorage.getItem('user');
+  if(!user) return null
+  return JSON.parse(user)
+}
+
+const initialState: UserState = {
+  user: getUserFromLocalStorage()
 };
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    loginUser:(state, action:PayloadAction<User>)=>{
+      const user = action.payload
+      state.user = user
+      localStorage.setItem('user', JSON.stringify('user'))
+
+      if(user.username === 'demo user'){
+        toast({ description: 'Welcome Guest'});
+        return;
+      }
+      toast({description: 'Login Successful' });
+    },
+    logoutUser:(state)=>{
+      state.user = null
+      localStorage.removeItem('user');
+    },
+
+  },
 });
+
+export const {loginUser, logoutUser} = userSlice.actions
 
 export default userSlice.reducer;
