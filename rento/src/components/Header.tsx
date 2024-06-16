469e1c0bd6ef6170c837e0b3dc3a25b69fda4a75
Omar-Alme <omar.almehdar17@gmail.com>
2024-06-17 00:13:37 +0200

    fix header component

--- rento/src/components/Header.tsx
+++ rento/src/components/Header.tsx
@@ -1,19 +1,27 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
+import { useAppSelector, useAppDispatch } from '@/hooks';
 
-function Header() {
+import { logoutUser } from '../features/user/userSlice';
+import { clearCart } from '../features/cart/cartSlice';
+import { useToast } from './ui/use-toast';
 
+
+
+const Header = () => {
+    const dispatch = useAppDispatch();
+    const { toast } = useToast();
     const navigate = useNavigate();
+    const user = useAppSelector((state) => state.userState.user);
 
-    const [user, setUser] = useState< {username:string} | null>({
-        username: 'demo user',
-    });
 
     const handleLogout = () => {
-        setUser(null);
+        dispatch(clearCart());
+        dispatch(logoutUser());
+        toast({ description: 'Logged out' });
         navigate('/');
     };
+
     return ( 
     <header>
         <div className='align-element flex justify-center sm:justify-end py-2'>
